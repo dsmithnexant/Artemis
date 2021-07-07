5c444bb8cb8606b73ebcd4e8f05bfae68aa7935f
Dan Smith <dsmith@nexant.com>
2021-07-07 09:07:36 -0400

    add amplify

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME",
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "Yhttps://hmseb5az9d.execute-api.us-west-2.amazonaws.com/06/devReturnModels",
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_13Zfxy52S",
+      APP_CLIENT_ID: "1h2d5t5seb5fail9k28jpelnhu",
+    },
+  };
+  
+  export default config;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
