c4372d5f834883b5d2a1da009a88ec199506c07b
Dan Smith <dsmith@nexant.com>
2021-07-09 17:14:28 -0400

    mapping promise to list item react

--- src/containers/Models.js
+++ src/containers/Models.js
@@ -7,6 +7,7 @@ import { onError } from "../libs/errorLib";
 import config from "../config";
 import { s3Upload } from "../libs/awsLib";
 import { API } from "aws-amplify";
+import { Storage } from "aws-amplify";
 
 export default function Models() {
   const file = useRef(null);
@@ -22,18 +23,17 @@ export default function Models() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
-        `Please pick a file smaller than ${
-          config.MAX_ATTACHMENT_SIZE / 1000000
+        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000
         } MB.`
       );
       return;
     }
-  
+
     setIsLoading(true);
-  
+
     try {
       const attachment = file.current ? await s3Upload(file.current) : null;
       history.push("/success");
@@ -42,29 +42,46 @@ export default function Models() {
       setIsLoading(false);
     }
   }
+
+  var bucketlist = Storage.list('', { level: 'private' })
+    .then((result) => {return result})
+    .catch(err => console.log(err));
+
+  /* bucketlist is returning as Promise<pending>. Need to extract value from asynchronous 
+     Promise, then map that to list items 
+  */
+
+  console.log(bucketlist);
+
+  var items = [{ key: "key1", eTag: "value1" }, { key: "key2", eTag: "value2" }];
+
   return (
     <div className="Models">
       <div className="lander">
         <h1>Project Artemis Models</h1>
         <p className="text-muted">Below is an up to date list of models that exist.</p>
+        {/* <ul>
+          {bucketlist.map((item) =>
+            <li key={item.key}>{item.key}</li>)}
+        </ul> */}
       </div>
       <div className="lander">
         <h4>If you would like to create a new model upload data here </h4>
         <p className="text-muted">Select your training data to upload.</p>
         <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="file">
-          <Form.Control onChange={handleFileChange} type="file" />
-        </Form.Group>
-        <LoaderButton
-          block
-          type="submit"
-          size="lg"
-          variant="primary"
-          isLoading={isLoading}
-          disabled={isDisabled}
-        >
-          Submit File to generate a new Model for a New Project
-        </LoaderButton>
+          <Form.Group controlId="file">
+            <Form.Control onChange={handleFileChange} type="file" />
+          </Form.Group>
+          <LoaderButton
+            block
+            type="submit"
+            size="lg"
+            variant="primary"
+            isLoading={isLoading}
+            disabled={isDisabled}
+          >
+            Submit File to generate a new Model for a New Project
+          </LoaderButton>
         </Form>
       </div>
     </div>
