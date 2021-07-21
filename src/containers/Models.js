d6a0be111f8f87dc9c56cdb583a6a3ca1e4dd828
Dan Smith <dsmith@nexant.com>
2021-07-21 11:07:11 -0400

    update formatting

--- src/containers/Models.js
+++ src/containers/Models.js
@@ -11,6 +11,11 @@ import { Storage } from "aws-amplify";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 export default function Models() {
@@ -80,37 +85,110 @@ export default function Models() {
         return (<ListItem key={link.key}>{link.key}</ListItem>);
       } 
     });
+
+
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+  }));
+    
+  const classes = useStyles();
+
   return (
     <div className="Models">
+
       <div className="lander">
         <h1>Project Artemis Models</h1>
-        <p className="text-muted">Below is an up to date list of models that exist in the system.</p>
-        <h4>If you would like to create a new model upload data here </h4>
-        <h10 className="text-muted">File should be named <b>DIGITS-DIGITS.csv</b> </h10>
-        <p className="text-muted">Select your training data to upload.</p>
-      <div className="list">
-        <List style={{maxHeight: 200, overflow: 'auto'}}>
-         {listItems} 
-        </List>
       </div>
-      </div>
-      <div className="lander">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="file">
-            <Form.Control onChange={handleFileChange} type="file" />
-          </Form.Group>
-          <LoaderButton
-            block
-            type="submit"
-            size="lg"
-            variant="primary"
-            isLoading={isLoading}
-            disabled={isDisabled}
-          >
-            Submit Training Data to Generate A New Model for A New Project
-          </LoaderButton>
-        </Form>
-      </div>
+      <div className={classes.root}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}> <b>Creating New Models</b></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Below is a list of all the models that exist in the Project Artemis ecosystem. If you would like to create a 
+            new model for a new project follow the directions below.
+            <br></br>
+            <div className="list">
+              <List style={{maxHeight: 200, overflow: 'auto'}}>
+              {listItems} 
+              </List>
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}><b>Naming Your Training Data</b></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            The project Artemis data pipeline is developed to only accept file uploads following a specific nomenclature.
+            First, all files uploaded must have the extension type <b>.csv</b>. 
+            <br></br>
+            <br></br>
+            Files should be named using the following convention where DIGITS refers to any numeric value <b>[0-9]</b> and CHARACTERS refers to <b>[a-z]</b>.
+            <br></br>
+            <br></br>
+             <b>DIGITS-DIGITS.csv</b>
+            <br></br>
+            <br></br>
+             <i>Example: 18-0003.csv</i>
+            <br></br>
+            <br></br>
+            If any of these rules are not followed you will recieve an alert message and will <b>NOT</b> be able to upload
+            your file.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}><b>Questions?</b></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            If you have any questions or suggestions regarding project Artemis reach out to
+             <b> dsmith@nexant.com </b>for further detail.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+    <br></br>
+    <br></br>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="file">
+        <Form.Control onChange={handleFileChange} type="file" />
+      </Form.Group>
+      <LoaderButton
+        block
+        type="submit"
+        size="lg"
+        variant="primary"
+        isLoading={isLoading}
+        disabled={isDisabled}
+      >
+        Submit Training Data to Generate A New Model for A New Project
+      </LoaderButton>
+    </Form> 
     </div>
   );
 }
