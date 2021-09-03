06ed6b3d09a1e67d752e4e7dd5b8a7cf314a603d
Dan Smith <dsmith@resource-innovations.com>
2021-09-03 14:45:44 -0400

    added app client id

--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ const config = {
       REGION: "us-west-2",
       USER_POOL_ID: "us-west-2_PTFV8y24N",
       IDENTITY_POOL_ID: "us-west-2:54ba1cec-eb84-4f1f-9741-2290752de104",
+      APP_CLIENT_ID: "44hu61mk3kg565s6gkbt8llgag",
       //USER_POOL_ID: "us-west-2_13Zfxy52S",  //test env
       //USER_POOL_ID: "us-west-2_2ktmxFnCJ",
       //APP_CLIENT_ID: "1h2d5t5seb5fail9k28jpelnhu", //test env 
