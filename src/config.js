912820ede9ac112204a43376e0c8c2fe2b257ca6
Dan Smith <dsmith@resource-innovations.com>
2021-09-03 14:13:15 -0400

    add production env configs for cognito and s3 bucket

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ const config = {
     MAX_ATTACHMENT_SIZE: 50000000000,
     s3: {
       REGION: "us-west-2",
-      BUCKET: "prod-orion-landing-bucket",
+      //BUCKET: "prod-orion-landing-bucket", // test env
+      BUCKET: "prod-artemis-landing-bucket",
       //REGION: "us-west-2",
       //BUCKET: "dev-orion-models",
     },
@@ -12,11 +13,13 @@ const config = {
     },
     cognito: {
       REGION: "us-west-2",
-      USER_POOL_ID: "us-west-2_13Zfxy52S",
+      USER_POOL_ID: "us-west-2_PTFV8y24N",
+      IDENTITY_POOL_ID: "us-west-2:54ba1cec-eb84-4f1f-9741-2290752de104",
+      //USER_POOL_ID: "us-west-2_13Zfxy52S",  //test env
       //USER_POOL_ID: "us-west-2_2ktmxFnCJ",
-      APP_CLIENT_ID: "1h2d5t5seb5fail9k28jpelnhu",
+      //APP_CLIENT_ID: "1h2d5t5seb5fail9k28jpelnhu", //test env 
       //APP_CLIENT_ID: "45lp43bgvolrm177hhnicjhlae",
-      IDENTITY_POOL_ID: "us-west-2:49482d7c-aa5a-4137-b865-f20c648c3732",
+      //IDENTITY_POOL_ID: "us-west-2:49482d7c-aa5a-4137-b865-f20c648c3732", //test env
     },
   };
   
